0a22d414d84b84ff7c4a0165810122dc1d4fbfb1
Daniel Kaminski <daniel.kaminski1998@gmail.com>
2025-04-07 00:06:23 +0200

    acceptance tests

---
+++ maths/clockface/clockface.go
@@ -0,0 +1,64 @@
+package clockface
+
+import (
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"time"
+)
+
+const (
+	secondHandLength = 90
+	clockCentreX     = 150
+	clockCentreY     = 150
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(t time.Time) Point {
+	p := secondHandPoint(t)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}
+	return p
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / float64(t.Second())))
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+	return Point{x, y}
+}
+
+func main() {
+	t := time.Now()
+	sh := SecondHand(t)
+	io.WriteString(os.Stdout, svgStart)
+	io.WriteString(os.Stdout, bezel)
+	io.WriteString(os.Stdout, secondHandTag(sh))
+	io.WriteString(os.Stdout, svgEnd)
+}
+
+func secondHandTag(p Point) string {
+	return fmt.Sprintf(`<line x1="150" y1="150" x2="%f" y2="%f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+}
+
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
